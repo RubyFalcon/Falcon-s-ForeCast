71ad0a9eee19ae6ad875712d81aaca47bd808c81
Tupay Felkin <61619620+RubyFalcon@users.noreply.github.com>
2025-02-17 18:30:23 +0000

    added Input to query the api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import "./styles.css";
 
+const countryInput = document.getElementById("country");
+const locationButton = document.getElementById("search-button");
 const getWeather = async (location) => {
   try {
-    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}n?key=NJVNLA2GU3RGV646GKCNDHHYX`;
-
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=NJVNLA2GU3RGV646GKCNDHHYX`;
     const response = await fetch(url);
     const data = await response.json();
     console.log(data);
@@ -13,3 +14,10 @@ const getWeather = async (location) => {
 };
 
 getWeather("london");
+
+locationButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log(countryInput.value);
+
+  getWeather(countryInput.value);
+});
