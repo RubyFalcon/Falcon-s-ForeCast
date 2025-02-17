b1aafb8f66a511027376563969840b18e86498e9
Tupay Felkin <61619620+RubyFalcon@users.noreply.github.com>
2025-02-17 18:08:36 +0000

    add Visual Crossing API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import "./styles.css";
+
+const getWeather = async (location) => {
+  try {
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}n?key=NJVNLA2GU3RGV646GKCNDHHYX`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getWeather("london");
